69f88b92cf24e01aa89066907c9ed21d8b867a56
Paula Gates <paulacgates@gmail.com>
2023-10-09 20:58:06 -0300

    Remove funcionalidade desnecessaria de remove

--- lib/repositories/imc_repository.dart
+++ lib/repositories/imc_repository.dart
@@ -8,11 +8,6 @@ class ImcRepository {
     _imcs.add(imc);
   }
 
-  Future<void> remove(String id) async {
-    await Future.delayed(const Duration(milliseconds: 100));
-    _imcs.remove(_imcs.where((tarefa) => tarefa.id == id).first);
-  }
-
   Future<List<Imc>> listar() async {
     await Future.delayed(const Duration(milliseconds: 100));
     return _imcs;
